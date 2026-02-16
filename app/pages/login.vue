d6166abf3ba882f88a9f77d9b8995a2b7956b3f9
Bruno Alarcão <brunno.cesar@live.com>
2026-02-16 18:37:43 -0300

    feat(template): Icons now provided by nuxt-icons

--- app/pages/login.vue
+++ app/pages/login.vue
@@ -41,19 +41,7 @@
               class="absolute left-3 top-1/2 -translate-y-1/2 text-text-secondary pointer-events-none"
               aria-hidden="true"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                class="w-5 h-5"
-              >
-                <path
-                  d="M3 4a2 2 0 00-2 2v1.161l8.441 4.221a1.25 1.25 0 001.118 0L19 7.162V6a2 2 0 00-2-2H3z"
-                />
-                <path
-                  d="M19 8.839l-7.77 3.885a2.75 2.75 0 01-2.46 0L1 8.839V14a2 2 0 002 2h14a2 2 0 002-2V8.839z"
-                />
-              </svg>
+              <Icon name="heroicons:envelope" class="w-5 h-5" />
             </span>
             <input
               id="email"
@@ -75,18 +63,7 @@
               class="absolute left-3 top-1/2 -translate-y-1/2 text-text-secondary pointer-events-none"
               aria-hidden="true"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                class="w-5 h-5"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              <Icon name="heroicons:lock-closed" class="w-5 h-5" />
             </span>
             <input
               id="password"
@@ -102,35 +79,16 @@
               :aria-label="showPassword ? 'Ocultar senha' : 'Mostrar senha'"
               @click="showPassword = !showPassword"
             >
-              <svg
+              <Icon
                 v-if="showPassword"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
+                name="heroicons:eye-slash"
                 class="w-5 h-5"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3.28 2.22a.75.75 0 00-1.06 1.06l14.5 14.5a.75.75 0 101.06-1.06l-1.322-1.323a7.012 7.012 0 003.3-3.3L19 10v1a.75.75 0 01-1.5 0v-1a.75.75 0 01-.181-.513 7.012 7.012 0 00-3.3-3.3L15 4.56V4a.75.75 0 01-1.5 0v.06l-1.97 1.97a9.916 9.916 0 00-2.56-.34c-2.6 0-5.02.97-6.94 2.64L3.28 2.22zM7 6.06l.94.94A2.5 2.5 0 017 6.06zm-2.72 5.12a9.92 9.92 0 001.46 1.46l1.37-1.37a4 4 0 015.66 5.66l1.37-1.37a9.92 9.92 0 001.46-1.46.75.75 0 000-1.06l-.01-.01a.75.75 0 00-1.06 0 8.44 8.44 0 01-1.43 1.2 7.96 7.96 0 01-1.2-1.43.75.75 0 00-1.06-.01l-.01.01a.75.75 0 000 1.06z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <svg
+              />
+              <Icon
                 v-else
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
+                name="heroicons:eye"
                 class="w-5 h-5"
-              >
-                <path
-                  d="M10 12.5a2.5 2.5 0 100-5 2.5 2.5 0 000 5z"
-                />
-                <path
-                  fill-rule="evenodd"
-                  d="M.664 10.59a1.651 1.651 0 010-1.186A10.004 10.004 0 0110 3c4.257 0 7.893 2.66 9.336 6.41.147.381.146.804 0 1.186A10.004 10.004 0 0110 17c-4.257 0-7.893-2.66-9.336-6.41zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
-                  clip-rule="evenodd"
-                />
-              </svg>
+              />
             </button>
           </div>
           <div class="flex justify-end mt-1.5">
